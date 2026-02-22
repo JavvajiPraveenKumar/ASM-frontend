99d7df78a995c541459121ff73cbfd5a6007d349
JavvajiPraveenKumar <praveenjavvaji6552@gmail.com>
2026-02-22 12:15:39 +0530

    modified the api response

---
+++ src/services/product.service.ts
@@ -0,0 +1,34 @@
+import api from "@/lib/api";
+import { Product } from "@/types";
+import { ProductResponse } from "@/types";
+export const ProductService = {
+    // Get all products
+    getProducts: async ():Promise<ProductResponse>=> {
+        const response = await api.get<ProductResponse>("/spare-parts");
+        return response.data;
+    },
+
+    // Get single product
+    getProductById: async (id: string) => {
+        const response = await api.get<Product>(`/products/${id}`);
+        return response.data;
+    },
+
+    // Create product
+    createProduct: async (data: Omit<Product, "id">) => {
+        const response = await api.post<Product>("/products", data);
+        return response.data;
+    },
+
+    // Update product
+    updateProduct: async (id: string, data: Partial<Product>) => {
+        const response = await api.patch<Product>(`/products/${id}`, data);
+        return response.data;
+    },
+
+    // Delete product
+    deleteProduct: async (id: string) => {
+        const response = await api.delete(`/products/${id}`);
+        return response.data;
+    },
+};
